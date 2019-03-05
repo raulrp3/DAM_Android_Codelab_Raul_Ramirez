4bb2be416de613925a68235f18d09c09be0bd729
Raul Ramirez <raul.ramirez@escuelaestech.es>
2019-03-05 13:00:31 +0100

    first commit

---
+++ app/src/main/java/com/example/codelabmaterialdesign/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.codelabmaterialdesign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("Tab 1"));
+        tabs.addTab(tabs.newTab().setText("Tab 2"));
+        tabs.addTab(tabs.newTab().setText("Tab 3"));
+    }
+}
