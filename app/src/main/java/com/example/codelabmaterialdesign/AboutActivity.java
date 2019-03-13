398fc2dca90a9f87647b0727fbefc9697288db5e
Raul Ramirez <raul.ramirez@escuelaestech.es>
2019-03-13 12:46:22 +0100

    end AboutActivity

--- app/src/main/java/com/example/codelabmaterialdesign/AboutActivity.java
+++ app/src/main/java/com/example/codelabmaterialdesign/AboutActivity.java
@@ -1,11 +1,15 @@
 package com.example.codelabmaterialdesign;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
+    TextView githubProfileText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,5 +17,21 @@ public class AboutActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar_about);
         setSupportActionBar(toolbar);
+
+        initUI();
+
+        githubProfileText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://github.com/raulrp3";
+                Intent intent = new Intent(AboutActivity.this,WebActivity.class);
+                intent.putExtra("url",url);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initUI(){
+        githubProfileText = findViewById(R.id.github_profile);
     }
 }
---
+++ app/src/main/java/com/example/codelabmaterialdesign/WebActivity.java
@@ -0,0 +1,32 @@
+package com.example.codelabmaterialdesign;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebActivity extends AppCompatActivity {
+
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+
+        initUI();
+
+        Intent intent = getIntent();
+        String url = intent.getStringExtra("url");
+        webView.loadUrl(url);
+    }
+
+    private void initUI(){
+        webView = findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+    }
+}
