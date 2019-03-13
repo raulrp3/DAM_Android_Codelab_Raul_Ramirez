cdc8f1b935e8a4231ffaabe701eaa6a84ffac254
raulrp3 <raul.ramirez@escuelaestech.es>
2019-03-13 18:51:17 +0100

    added link to phone

--- app/src/main/java/com/example/codelabmaterialdesign/AboutActivity.java
+++ app/src/main/java/com/example/codelabmaterialdesign/AboutActivity.java
@@ -1,6 +1,7 @@
 package com.example.codelabmaterialdesign;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 public class AboutActivity extends AppCompatActivity {
 
     TextView githubProfileText;
+    TextView telephone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,15 +25,37 @@ public class AboutActivity extends AppCompatActivity {
         githubProfileText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String url = "https://github.com/raulrp3";
-                Intent intent = new Intent(AboutActivity.this,WebActivity.class);
-                intent.putExtra("url",url);
+                loadPage(R.id.github_profile);
+            }
+        });
+
+        telephone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phone = "tel:" + telephone.getText().toString().trim();
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(phone));
                 startActivity(intent);
             }
         });
     }
 
     private void initUI(){
+
         githubProfileText = findViewById(R.id.github_profile);
+        telephone = findViewById(R.id.telephone);
+    }
+
+    private void loadPage(int id){
+        Intent intent = new Intent(AboutActivity.this,WebActivity.class);
+        switch (id){
+            case R.id.github_profile:{
+                String url = "https://github.com/raulrp3";
+                intent.putExtra("url",url);
+            }
+            break;
+        }
+        startActivity(intent);
     }
 }
